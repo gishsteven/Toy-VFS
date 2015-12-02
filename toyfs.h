2cc62abd39ebdf6acd1479ab914e2838bcfd591b
kevin-gadek <kgadek@sbcglobal.net>
2015-12-01 18:01:05 -0800

    inode def in header

--- toyfs.h
+++ toyfs.h
@@ -1,6 +1,12 @@
 #include <linux/types.h>
 const int SUPERBLOCK_DEFAULT_SIZE = 4096;
 
+struct toyfs_inode {
+    mode_t mode;
+    uint32_t inode_num, block_num;
+    uint64_t child_count, file_size;
+};
+
 //uint32_t better data type to use than unsigned int at this point
 struct toyfs_super_block {
 	int id;
