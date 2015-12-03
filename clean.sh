3b4a45027ef31731f50792be7522854adc67a498
Nathan Sanders <nawasaca@gmail.com>
2015-12-03 13:35:42 -0800

    Adding init.sh, clean.sh, and .gitignore file

---
+++ clean.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+FS_TYPE="toyfs"
+MODULE_FILE="toyfs_mod.ko"
+MODULE_NAME="toyfs_mod"
+DISK_FILEPATH="/tmp/toyfs_disk.img"
+MOUNT_DIR="/mnt/$FS_TYPE"
+
+# Initialization must be run as root user
+if [[ $EUID -ne 0 ]]; then
+   echo "Usage: sudo ./init.sh" 1>&2
+   exit 1
+fi
+
+# Unmount the disk
+umount $DISK_FILEPATH
+
+# Unload kernel module
+rmmod $MODULE_NAME
+
+# Clean directory
+make clean
+
+# Remove Disk
+rm $DISK_FILEPATH
---
+++ init.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+TIME=2
+FS_TYPE="toyfs"
+MODULE_FILE="toyfs_mod.ko"
+MODULE_NAME="toyfs_mod"
+DISK_FILEPATH="/tmp/toyfs_disk.img"
+MOUNT_DIR="/mnt/$FS_TYPE"
+
+# Initialization must be run as root user
+if [[ $EUID -ne 0 ]]; then
+   echo "Usage: sudo ./init.sh" 1>&2
+   exit 1
+fi
+
+# Create 4.0GB disk to mount as filesystem
+time dd if=/dev/zero of=$DISK_FILEPATH bs=4096 seek=1048576 count=0
+
+# Compile updates
+make
+
+# Load updated kernel module
+insmod $MODULE_FILE
+
+# Check filesystem registration
+if cat /proc/filesystems | grep $FS_TYPE ; then
+	echo "* Filesystem is registered in /proc/filesystems"
+else
+	echo "Warning: Filesystem not registered in /proc/filesystems "
+fi
+
+# Mount disk image to directory
+mkdir -p $MOUNT_DIR
+mount -o loop -t $FS_TYPE $DISK_FILEPATH $MOUNT_DIR
